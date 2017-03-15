914e14819c83a3b13daac68b284fa317380064c7
Raphael Schwarz <raphael@codeworx.at>
2017-03-15 15:10:19 +0100

    UnitTesting

--- tools/build.cmd
+++ tools/build.cmd
@@ -1,5 +1,4 @@
 @echo off
-set DOTNET_BUILD_STRONG_NAME_KEYFILE=..\..\private\signkey.snk
 
 del .\nuget\*.nupkg
 
@@ -16,6 +15,7 @@ dotnet msbuild ../Lucile.sln /p:Configuration=Release;SignAssembly=true;Assembly
 dotnet pack ../src/Lucile.Primitives --no-build -c Release -o ..\..\tools\nuget\ --include-source --version-suffix %%~nf
 dotnet pack ../src/Lucile.Core --no-build -c Release -o ..\..\tools\nuget\ --include-source --version-suffix %%~nf
 dotnet pack ../src/Lucile.EntityFrameworkCore --no-build -c Release -o ..\..\tools\nuget\ --include-source --version-suffix %%~nf
+dotnet pack ../src/Lucile.ServiceModel --no-build -c Release -o ..\..\tools\nuget\ --include-source --version-suffix %%~nf
 
 )
 
