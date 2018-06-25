f978046118834d913ef05c1200d8dc6dccb33666
Raphael Schwarz <raphael@codeworx.at>
2018-06-25 13:50:34 +0200

    Fix JsonInheritanceConverter Bug

--- src/Lucile.Primitives/Json/JsonInheritanceConverter.cs
+++ src/Lucile.Primitives/Json/JsonInheritanceConverter.cs
@@ -1,6 +1,50 @@
-﻿namespace Lucile.Json
+﻿using System;
+
+namespace Lucile.Json
 {
     public class JsonInheritanceConverter
     {
+        internal static readonly string DefaultDiscriminatorName = "discriminator";
+
+        private readonly Type _baseType;
+        private readonly string _discriminator;
+        private readonly bool _readTypeProperty;
+
+        public JsonInheritanceConverter()
+
+            : this(DefaultDiscriminatorName, false)
+        {
+        }
+
+        public JsonInheritanceConverter(string discriminator)
+
+            : this(discriminator, false)
+        {
+        }
+
+        public JsonInheritanceConverter(string discriminator, bool readTypeProperty)
+        {
+            _discriminator = discriminator;
+            _readTypeProperty = readTypeProperty;
+        }
+
+        public JsonInheritanceConverter(Type baseType)
+
+            : this(baseType, DefaultDiscriminatorName)
+        {
+        }
+
+        public JsonInheritanceConverter(Type baseType, string discriminator)
+            : this(discriminator, false)
+        {
+            _baseType = baseType;
+        }
+
+        public virtual string DiscriminatorName => _discriminator;
+
+        public virtual string GetDiscriminatorValue(Type type)
+        {
+            return type.Name;
+        }
     }
 }
