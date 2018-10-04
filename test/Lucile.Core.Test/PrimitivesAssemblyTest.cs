f052e4306351b5aecfa08f410d0ac2d0f780c909
Raphael Schwarz <raphael@codeworx.at>
2018-10-04 13:54:33 +0200

    reduce Dynamic Tuple Propperty

---
+++ test/Lucile.Core.Test/PrimitivesAssemblyTest.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Lucile.Data;
+using Xunit;
+
+namespace Tests
+{
+    public class PrimitivesAssemblyTest
+    {
+        [Fact]
+        public void LoadPrimitivesAssembly()
+        {
+            var test = typeof(DynamicTuple).Assembly;
+            Assert.NotEmpty(test.GetTypes());
+        }
+    }
+}
