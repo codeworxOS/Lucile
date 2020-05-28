be90a7a0d50655737640dea9901e753ddeae00c7
Raphael Schwarz <raphael@codeworx.at>
2020-05-28 11:20:52 +0200

    Major Version update

---
+++ test/Lucile.Core.Test/Metadata/ContactValueAccessor.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using Lucile.Data.Metadata;
+
+namespace Lucile.Core.Test.Metadata
+{
+    public class ContactValueAccessor : IEntityValueAccessor
+    {
+        public Action<object, object> CreateAddItemDelegate(INavigationProperty navigationProperty)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Func<object, object> CreateGetValueDelegate(string propertyName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Action<object, object> CreateRemoveItemDelegate(INavigationProperty navigationProperty)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Action<object, object> CreateSetValueDelegate(string propertyName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Func<object, object, bool> CreatMatchForeignKeyDelegate(INavigationProperty navigationProperty)
+        {
+            throw new NotImplementedException();
+        }
+
+        public object GetPrimaryKey(object entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Type GetPrimaryKeyType()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool IsOfType(object value)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool KeyEquals(object leftEntity, object rightEntity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ test/Lucile.Core.Test/MetadataModelCustomAccessorTest.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Lucile.Core.Test.Metadata;
+using Lucile.Data.Metadata;
+using Lucile.Data.Metadata.Builder;
+using Lucile.Test.Model;
+using Xunit;
+
+namespace Lucile.Core.Test
+{
+    public class MetadataModelCustomAccessorTest
+    {
+
+        [Fact]
+        public void TestCustomAccessor_ExpectsNoError()
+        {
+            var builder = new MetadataModelBuilder();
+            builder.Entity<Article>();
+            builder.Entity<ArticleName>();
+            builder.Entity<Contact>();
+
+            var model = builder.ApplyConventions().ToModel(new CustomAccessorFactory());
+
+            Assert.NotNull(model);
+        }
+    }
+}
