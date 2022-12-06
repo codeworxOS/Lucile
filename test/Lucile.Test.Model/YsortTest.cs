79c43a19f420a0cb650bb20904baca7e19ec6c99
Raphael Schwarz <raphael@codeworx.at>
2022-12-06 17:10:26 +0100

    sort Test

---
+++ test/Lucile.Test.Model/YsortTest.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Lucile.Test.Model
+{
+    public class YsortTest
+    {
+        public Guid Id { get; set; }
+
+        public string JFirstProperty { get; set; }
+
+        public int YSecondProperty { get; set; }
+    }
+}
